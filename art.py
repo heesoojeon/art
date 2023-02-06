45f8514419367dd3c0ab9c316e7099d2eea09666
heesoojeon <a01023648978@gmail.com>
2023-02-06 22:05:21 +0900

    work

--- art.py
+++ art.py
@@ -1,5 +1,11 @@
 print ('hello')
 print("안녕하세요 청표범입니다")
+pip install pillow
+from PIL import Image
+
+def pixelate(input_file_path, pixel_size):
+    image = Image.open(input_file_path)
+    image = image.resize
 
 
 
