6e57ad699fa69cdd8d786c3f7fed5569c581bb50
heesoojeon <a01023648978@gmail.com>
2023-02-07 21:31:55 +0900

    수정

--- art.py
+++ art.py
@@ -1,18 +1,1 @@
-print ('hello')
-print("안녕하세요 청표범입니다")
-pip install pillow
 from PIL import Image
-image = Image.open("C:/Users/user/Desktop/작업 블라썸.jpeg")
-image.show()
-
-def pixelate(input_file_path, pixel_size):
-    image = Image.open(input_file_path)
-    image = image.resize
-#PS C:\Users\user\Desktop\art> & C:/Users/user/AppData/Local/Programs/Python/Python310/python.exe c:/Users/user/Desktop/art/art.py
- # File "c:\Users\user\Desktop\art\art.py", line 3
-   # pip install pillow
-     #   ^^^^^^^
-#SyntaxError: invalid syntax
-#PS C:\Users\user\Desktop\art>=> 이런 에러메세지가 뜹니다.ㅜㅜ
-
-
