c77dfcf56613961aaedd365e63d70060becb2428
heesoojeon <a01023648978@gmail.com>
2023-02-09 02:01:27 +0900

    work

--- art2.py
+++ art2.py
@@ -1,14 +1,38 @@
+
+
 import cv2
-import numpy
 
-art = '작업블라썸.jpg'
-image = cv2.imread(art)
+file_name = '작업 블라썸.jpg'
+image = cv2.imread(file_name)
 
-panel = numpy.zeros([100,400],numpy.unit8)
+panel = np.zeros([100,400], np.uint8)
 cv2.namedWindow('panel')
 
 def nothing(x):
     pass
 
-cv2.createTrackbar('FX','panel',1,100,nothing)
-cv2.createTrackbar('FY','panel',1,100,nothing)
+cv2.createTrackbar('FX', 'panel', 1,100, nothing)
+cv2.createTrackbar('FY', 'panel', 1,100, nothing)
+
+return_name = file_name.replace('.','_pixel.')
+
+while True:
+    
+    
+    fx = cv2.getTrackbarPos('FX', 'panel') / 100
+    fy = cv2.getTrackbarPos('FY', 'panel') / 100
+
+    pixel = cv2.resize(image, dsize=None, fx=fx, fy=fy, interpolation=cv2.INTER_NEAREST)
+    pixel = cv2.resize(pixel, dsize=(int(image.shape[1]*1),int(image.shape[0]*1)), interpolation=cv2.INTER_NEAREST)
+    
+    cv2.imshow('image', image)
+    cv2.imshow('pixel', pixel)
+    cv2.imshow('panel', panel)
+    
+    if cv2.waitKey(1) == ord('s'):
+        cv2.imwrite(return_name, pixel)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
