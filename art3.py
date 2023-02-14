bda4da63d536cde439a71ae4b2280dc8e1ecfdf6
heesoojeon <a01023648978@gmail.com>
2023-02-14 22:07:16 +0900

    work

--- art3.py
+++ art3.py
@@ -3,15 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-image = cv2.imread("camu_cat.mp4")
-cap = cv2.VideoCapture("camu_cat.mp4")
-cap.set(1, 2) #2- the second frame of my video
-res, frame = cap.read()
-cv2.imshow("video", frame)
-while True:
-    ch = 0xFF & cv2.waitKey(1)
-    if ch == 27:
-        break
+Vid = cv2.VideoCapture('camu_cat.mp4')
+
+if Vid.isOpened():
+    fps = Vid.get(cv2.CAP_PROP_FPS)
+    f_count = Vid.get(cv2.CAP_PROP_FRAME_COUNT)
+    f_width = Vid.get(cv2.CAP_PROP_GIGA_FRAME_SENS_WIDTH)
+    f_height = Vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
     
-cv2.imshow("camu_cat.mp4")
+    print('Frames per second : ', fps,'FPS')
+    print('Frame count : ', f_count )
+    print('Frame width : ', f_width)
+    print('Frame height : ', f_height)
     
+while Vid.isOpened():
+    ret, frame = Vid.read()
+    if ret:
+        re_frame = cv2.resize(frame, (round(f_width/4),round(f_height/4)))
+        cv2.imshow('camu_cat', re_frame)
+        key = cv2.waitKey(10)
+        
+        if key == ord('q'):
+            break
+    else: break
+    
+Vid.release()
+cv2.destroyAllWindows()    
+
+
+
