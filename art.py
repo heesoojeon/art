b4149e3fa3e06de03622ef206156cdb216d651a0
heesoojeon <a01023648978@gmail.com>
2023-03-07 02:18:14 +0900

    wk

--- art.py
+++ art.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 os.chdir("C://Users//user//Desktop")
-work = Image.open('작업 블라썸.jpg')
+work = Image.open('work_blossom.jpg')
 work.show()
 work = Image.new("RGB",(500,500),(125,255,125))
 work1 = work.load()
