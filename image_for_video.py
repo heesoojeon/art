b7157f50bbb0e4195199c84e2b9ec2e797fdfe49
heesoojeon <a01023648978@gmail.com>
2023-03-11 06:58:57 +0900

    wk

---
+++ image_for_video.py
@@ -0,0 +1,25 @@
+import cv2
+import time
+import os
+
+def get_files(path):
+    for root, subdirs, files in os.walk(path):
+        
+        list_files = []
+        
+        if len(files) >0:
+            for f in files:
+                fullpath = root +'/'+f
+                list_files.append(fullpath)
+                
+    return list_files
+
+image_files = get_files('C:\Users\user\Desktop')
+
+img = cv2.imread(image_files[0])
+height,width,channel = img.shape
+fps = 25
+
+fource = cv2.VedeoWriter_fource(*'mp4v')
+BASE_DIR = os.path.dirname(os.path.abspath(_file))
+writer = cv2.VideoWriter(BASE_DIR + '/'+ 'output.mp4',fourcc, fps, (width,height))  
